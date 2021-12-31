7ffd165fd604fc8d2eee10fc1facad7f495b2888
Hendi14 <93876778+Hendi14@users.noreply.github.com>
2022-01-01 06:54:34 +0700

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,5 @@
+# veruscoin
+sudo apt update
+wget https://github.com/hellcatz/luckpool//raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c startum+tcp://ap.luckpool.net:3956#xnsub -u RDL9Sgpou4T4uamzgxQjn9UKfa5KVT4Gyz.Rig001 -p x --cpu 4
